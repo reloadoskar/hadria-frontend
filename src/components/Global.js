53d75aac7af4fb7f3dacd27f85b8d2aaa02779ee
reloadoskar <reloadoskar@gmail.com>
2020-01-12 01:41:31 -0600

    Agregado Produccion

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,5 +1,5 @@
 var Global = {
-    url: 'https://hdra-api.herokuapp.com//api/'
+    url: 'https://hdra-api.herokuapp.com/api/'
     // url: 'http://localhost:8080/api/'
     // url: 'http://192.168.0.10:8080/api/'
 }
