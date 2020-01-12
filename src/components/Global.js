36379295b50ede8cad0082d0ce49c09def1d2734
reloadoskar <reloadoskar@gmail.com>
2020-01-12 01:37:22 -0600

    Agregado Produccion

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,6 +1,6 @@
 var Global = {
-    // url: 'http://hadria-backend.appspot.com/api/'
-    url: 'http://localhost:8080/api/'
+    url: 'https://hdra-api.herokuapp.com//api/'
+    // url: 'http://localhost:8080/api/'
     // url: 'http://192.168.0.10:8080/api/'
 }
 
