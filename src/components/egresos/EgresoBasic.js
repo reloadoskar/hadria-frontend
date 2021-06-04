144cc5a764c26ec657adaa6eecb97437b14e096c
reloadoskar <reloadoskar@gmail.com>
2021-06-04 10:58:01 -0500

    Updated egresoBasic.js

--- src/components/egresos/EgresoBasic.js
+++ src/components/egresos/EgresoBasic.js
@@ -14,13 +14,16 @@ export default function VentaBasic(props){
         <div>
             {egreso === null ? null :
                 <Grid container spacing={1}>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={2}>
                         <Typography>{egreso.fecha}</Typography>
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Typography>{egreso.concepto}</Typography>
                     </Grid>
                     <Grid item xs={12} sm={4}>
+                        <Typography>{egreso.descripcion}</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={2}>
                         <Typography align="right">{formatNumber(egreso.importe,2)}</Typography>
                     </Grid>
                 </Grid>
