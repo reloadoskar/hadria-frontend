079a528482865585f45e3d454874bf01722c2afb
reloadoskar <reloadoskar@gmail.com>
2022-01-04 10:45:25 -0600

    update

--- src/components/compras/CompraBasic.js
+++ src/components/compras/CompraBasic.js
@@ -30,16 +30,23 @@ export default function CompraBasic(props){
     useEffect(() => {
         if(compraLocal){
             let tv = sumImporte(compraLocal.ventaItems)
+            compraLocal.totalVenta = tv
             let tg = sumImporte(compraLocal.gastos)
+            compraLocal.totalGastos = tg
             let tp = sumImporte(compraLocal.pagos)
+            compraLocal.totalPagos = tp
             let tc = compraLocal.importe
             setTotalVenta(tv) 
             setTotalGastos(tg)
             setTotalPagos(tp)
             if(compraLocal.tipoCompra.tipo === 'COMPRAS' ||compraLocal.tipoCompra.tipo === 'COMPRA' ){
-                setResultado(tv-tc-tg)
+                let re = tv-tc-tg
+                setResultado(re)
+                compraLocal.resultado = re
             }else{
-                setResultado(tv-tg-tp)
+                let re = tv-tg-tp
+                setResultado(re)
+                compraLocal.resultado = re
             }
             setEmpaques(sumEmpStock(compraLocal.items))
             setCantidad(sumStock(compraLocal.items))
@@ -65,87 +72,9 @@ export default function CompraBasic(props){
     return (
         <React.Fragment>
             {compraLocal === null ? null :
-            <Grid item container xs={12} justifyContent="center" alignItems="baseline">
-                <Grid item xs={3} sm={1}>
-                    <Typography className={classes.textoMiniFacheron}>{compraLocal.fecha}</Typography>
-                    <Typography className={classes.textoMirame} >#{compraLocal.folio}</Typography>
-                </Grid>
-                <Grid item xs={9} sm={4}>
-                    <Typography className={classes.textoMiniFacheron}>{compraLocal.clave} | {compraLocal.tipoCompra.tipo} </Typography>
-                    <Typography>{compraLocal.provedor.nombre}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={1} className={tEmpaques < 1 && tCantidad < 1 ? classes.suspended : classes.pro}>
-                    <Typography align="center" className={classes.textoMiniFacheron}>{tEmpaques < 1 && tCantidad < 1 ? "TERMINADO" : compraLocal.status}</Typography>
-                </Grid>
-                {compraLocal.tipoCompra.tipo === "COMPRAS" || compraLocal.tipoCompra.tipo === "COMPRA"  ?
-                    <React.Fragment>
-                        <Grid item xs={4} sm={2}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Venta:</Typography>
-                            <Typography align="right">
-                                ${formatNumber(totalVenta,2)} 
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={4} sm={1}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Costo:</Typography>
-                            <Typography align="right">$ {formatNumber(compraLocal.importe,2)}</Typography>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Pagado:</Typography>
-                            <Typography align="right">${formatNumber(totalPagos,2)}</Typography>
-                        </Grid>
-                        <Grid item xs={4} sm={1}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Gastos:</Typography>
-                            <Typography align="right">
-                                ${formatNumber(totalGastos,2)} 
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={1}>
-                            <Typography className={classes.textoMiniFacheron} align="right">Resultado:</Typography>
-                            <Typography align="right" className={resultado<0 ? classes.textoMirameSangron : classes.textoMirameExito}>
-                                ${formatNumber(resultado,2)} 
-                            </Typography>
-                        </Grid>
-                    </React.Fragment>
-                    :
-                    <React.Fragment>
-                        <Grid item xs={4} sm={2}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Venta:</Typography>
-                            <Typography align="right">${formatNumber(totalVenta,2)} </Typography>
-                        </Grid>
-                        <Grid item xs={4} sm={1}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Pagos:</Typography>
-                            <Typography align="right">
-                                ${formatNumber(totalPagos,2)} 
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={4} sm={1}>
-                            <Typography align="right" className={classes.textoMiniFacheron}>Gastos:</Typography>
-                            <Typography align="right">
-                                ${formatNumber(totalGastos,2)} 
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={1}>
-                            <Typography className={classes.textoMiniFacheron} align="right">Resultado:</Typography>
-                            <Typography align="right" className={resultado<0 ? classes.textoMirameSangron : classes.textoMirameExito}>
-                                ${formatNumber(resultado,2)} 
-                            </Typography>
-                        </Grid>
-                    </React.Fragment>
-                }
-                <Grid item xs={12} sm={1}>
+            <Grid container justifyContent="center" alignItems="baseline" spacing={1} className={classes.paperContorno}>
+                <Grid item xs={12}>
                     <Typography align="right">
-                        {/* <IconButton
-                            disabled
-                            size="small"
-                            onClick={() => handleRecuperarGastos(compra._id)}
-                            >
-                            <RefreshIcon />
-                        </IconButton>
-                        <IconButton
-                            disabled
-                            size="small"
-                            onClick={() => handleRecuperarVentas(compra._id)}
-                            >
-                            <RefreshIcon />
-                        </IconButton> */}
                         <Badge variant="dot" color="secondary">
                         <IconButton
                             size="small"
@@ -160,19 +89,77 @@ export default function CompraBasic(props){
                             >
                             <EditIcon />
                         </IconButton>
-                        {/* <IconButton
-                            size="small"
-                            disabled={compra.status === "CANCELADO" ? true : false}
-                            aria-label="delete"
-                            onClick={() => openConfirm(compra)}
-                            >
-                            <DeleteIcon />
-                        </IconButton> */}
                     </Typography>
                 </Grid>
+                <Divider />
+                <Grid item xs={3} sm={1}>
+                    <Typography className={classes.textoMirame} >#{compraLocal.folio}</Typography>
+                    <Typography 
+                        align="center" 
+                        className={ ` ${classes.textoMiniFacheron}  ${tEmpaques < 1 && tCantidad < 1 ? classes.suspended : classes.pro }` }>
+                            {tEmpaques < 1 && tCantidad < 1 ? "TERMINADO" : compraLocal.status}
+                    </Typography>
+                    <Typography className={classes.textoMiniFacheron}>{compraLocal.fecha}</Typography>
+                </Grid>
+                <Grid item xs={9} sm={3}>
+                    <Typography className={classes.textoMiniFacheron}>{compraLocal.clave} | {compraLocal.tipoCompra.tipo} </Typography>
+                    <Typography>{compraLocal.provedor.nombre}</Typography>
+                </Grid>
+                {compraLocal.tipoCompra.tipo === "COMPRAS" || compraLocal.tipoCompra.tipo === "COMPRA"  ?
+                    <React.Fragment>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Venta:</Typography>
+                            <Typography align="right">
+                                ${formatNumber(totalVenta,2)} 
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Costo:</Typography>
+                            <Typography align="right">$ {formatNumber(compraLocal.importe,2)}</Typography>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Pagado:</Typography>
+                            <Typography align="right">${formatNumber(totalPagos,2)}</Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Gastos:</Typography>
+                            <Typography align="right">
+                                ${formatNumber(totalGastos,2)} 
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={2}>
+                            <Typography className={classes.textoMiniFacheron} align="right">Resultado:</Typography>
+                            <Typography align="right" className={resultado<0 ? classes.textoMirameSangron : classes.textoMirameExito}>
+                                ${formatNumber(resultado,2)} 
+                            </Typography>
+                        </Grid>
+                    </React.Fragment>
+                    :
+                    <React.Fragment>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Venta:</Typography>
+                            <Typography align="right">${formatNumber(totalVenta,2)} </Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Pagos:</Typography>
+                            <Typography align="right">
+                                ${formatNumber(totalPagos,2)} 
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={2}>
+                            <Typography align="right" className={classes.textoMiniFacheron}>Gastos:</Typography>
+                            <Typography align="right">
+                                ${formatNumber(totalGastos,2)} 
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={2}>
+                            <Typography className={classes.textoMiniFacheron} align="right">Resultado:</Typography>
+                            <Typography align="right" className={resultado<0 ? classes.textoMirameSangron : classes.textoMirameExito}>
+                                ${formatNumber(resultado,2)} 
+                            </Typography>
+                        </Grid>
+                    </React.Fragment>
+                }                
             </Grid>
             }
-            <Divider />
         </React.Fragment>
     )
 }
--- src/components/compras/ListaCompras.jsx
+++ src/components/compras/ListaCompras.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import { CircularProgress, Grid, Typography, Button, Menu, MenuItem, Divider } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { CircularProgress, Grid, Typography, Divider } from '@material-ui/core'
 import CompraBasic from './CompraBasic'
 import { sumImporte } from '../Tools'
-import useStyles  from '../hooks/useStyles'
+import useStyles from '../hooks/useStyles'
 import CountUpAnimation from '../tools/CountUpAnimation'
-export default function ListaCompras({compras, editCompra, verCompra, recuperarVentas, recuperarGastos, meses, month, onChangeMonth}){
+export default function ListaCompras({ compras, editCompra, verCompra }) {
     const classes = useStyles()
     const [lasCompras, setLasCompras] = useState(null)
 
@@ -14,102 +14,71 @@ export default function ListaCompras({compras, editCompra, verCompra, recuperarV
     const [tGastos, setGastos] = useState(0)
     const [tResultado, setResultado] = useState(0)
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
     useEffect(() => {
-        if(compras){
+        if (compras) {
             setLasCompras(compras)
         }
         return () => setLasCompras(null)
-    },[compras])
+    }, [compras])
 
-    useEffect(()=>{
-        if(lasCompras){
+    useEffect(() => {
+        let isLoaded = true
+        if (lasCompras && isLoaded) {
             let tc = 0
             tc = sumImporte(lasCompras)
             setCosto(tc)
             let tv = 0
-            lasCompras.map(compra=> tv += sumImporte(compra.ventaItems))
+            lasCompras.map(compra => tv += sumImporte(compra.ventaItems))
             setVentas(tv)
             let tg = 0
-            lasCompras.map(compra=> tg += sumImporte(compra.gastos))
+            lasCompras.map(compra => tg += sumImporte(compra.gastos))
             setGastos(tg)
             let tp = 0
-            lasCompras.map(compra=> tp += sumImporte(compra.pagos))
+            lasCompras.map(compra => tp += sumImporte(compra.pagos))
             setPagos(tp)
             let tr = tv - tc - tg
             setResultado(tr)
         }
-        return () => clearTotals()
-    },[lasCompras])
-    const clearTotals = () => {
+        return () => isLoaded = false
+    }, [lasCompras])
+
+    const clearComponent = () => {
         setCosto(0)
         setVentas(0)
+        setPagos(0)
+        setGastos(0)
         setResultado(0)
     }
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    }
-    const handleChange = (mes) => {
-        setLasCompras(null)
-        onChangeMonth(mes)
-        handleClose()
-    }
-    return(
-        <Grid item container xs={12} spacing={2}>
+    
+    return (
+        <Grid item container xs={12} spacing={2} >
+            <Grid item xs={12} sm={1}>
+                <Typography variant="h3" align="center">{lasCompras ? <CountUpAnimation num={lasCompras.length} temp={300} /> : null}</Typography>
+                <Typography align="center">Operaciones</Typography>
+            </Grid>
             <Grid item xs={12} sm={3}>
-                        <Button 
-                            fullWidth
-                            onClick={handleClick}
-                            className={classes.botonsoteGenerico} 
-                            children={
-                                meses[month]
-                        }/>
-                        <Menu 
-                            anchorEl={anchorEl}
-                            keepMounted
-                            open={Boolean(anchorEl)}
-                            getContentAnchorEl={null}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'center',
-                              }}
-                              transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'center',
-                              }}
-                            onClose={handleClose}
-                        >
-                            {meses.map((mes,i)=>(
-                                <MenuItem onClick={()=>handleChange(i)} key={i}>{mes}</MenuItem>
-                            ))}
-                        </Menu>
-                </Grid>
-                <Grid item xs={12} sm={1}>
-                    <Typography variant="h2" align="center">{lasCompras ? <CountUpAnimation num={lasCompras.length} temp={300} /> : null}</Typography>
-                    <Typography align="center">Operaciones</Typography>
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                    <Typography align="right">Venta total</Typography>
-                    <Typography variant="h5" align="right">$ <CountUpAnimation num={tVentas} temp={650} /></Typography>
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                    <Typography align="right">Costo total</Typography>
-                    <Typography align="right" variant="h5" className={classes.textoMirameSangron}>$<CountUpAnimation num={tCosto} temp={900} /></Typography>
-                    <Typography align="right" variant="h6">Pagos: -$<CountUpAnimation num={tPagos} temp={520} /></Typography>
-                    <Divider />
-                    <Typography align="right" variant="h6">Saldo: $<CountUpAnimation num={tCosto-tPagos} temp={500} /></Typography>
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                    <Typography align="right">Gasto total</Typography>
-                    <Typography variant="h5" align="right">-$<CountUpAnimation num={tGastos} temp={560}/></Typography>
-                </Grid>
-                <Grid item xs={12} sm={2}>
-                    <Typography align="right">Resultado</Typography>
-                    <Typography align="right" variant="h5" className={tResultado > 0 ? classes.textoMirameExito : classes.textoMirameSangron}>$<CountUpAnimation num={tResultado} temp={230} /></Typography>
-                </Grid>
+                <Typography align="right">Venta total</Typography>
+                <Typography variant="h5" align="right" className={classes.textoMirame}>$ <CountUpAnimation num={tVentas} temp={650} /></Typography>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+                <Typography align="right">Costo total</Typography>
+                <Typography align="right" variant="h5" className={classes.textoMirameSangron}>$<CountUpAnimation num={tCosto} temp={900} /></Typography>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+                <Typography align="right">Pagos</Typography>
+                <Typography align="right" variant="h6">-$<CountUpAnimation num={tPagos} temp={520} /></Typography>
+                <Divider />
+                <Typography align="right">Saldo</Typography>
+                <Typography align="right" variant="h6">$<CountUpAnimation num={tCosto - tPagos} temp={500} /></Typography>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+                <Typography align="right">Gasto total</Typography>
+                <Typography variant="h5" align="right">-$<CountUpAnimation num={tGastos} temp={560} /></Typography>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+                <Typography align="right">Resultado</Typography>
+                <Typography align="right" variant="h5" className={tResultado > 0 ? classes.textoMirameExito : classes.textoMirameSangron}>$<CountUpAnimation num={tResultado} temp={230} /></Typography>
+            </Grid>
             {lasCompras === null ?
                 <Grid item xs={12}>
                     <Typography component="div" align="center" >
@@ -119,20 +88,18 @@ export default function ListaCompras({compras, editCompra, verCompra, recuperarV
                 :
                 <React.Fragment>
                     {lasCompras
-                    .map((compra, i) => (
-                        <Grid item xs={12} key={i}>
-                            <CompraBasic 
-                                key={i} 
-                                compra={compra} 
-                                editCompra={editCompra}  
-                                verCompra={verCompra}
-                                recuperarVentas={recuperarVentas}
-                                recuperarGastos={recuperarGastos}
-                            />
-                        </Grid>
-                    ))
+                        .map((compra, i) => (
+                            <Grid item xs={12} key={i}>
+                                <CompraBasic
+                                    key={i}
+                                    compra={compra}
+                                    editCompra={editCompra}
+                                    verCompra={verCompra}
+                                />
+                            </Grid>
+                        ))
                     }
-                
+
                 </React.Fragment>
             }
         </Grid>
